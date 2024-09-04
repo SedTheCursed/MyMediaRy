fc701f778c11472c778946a03fea7799b181db7d
sedthecused <sed.robert@gmail.com>
2024-09-04 15:02:53 +0200

    WoofActivity for design references

--- app/src/main/java/com/brosedda/mymediary/ui/theme/Type.kt
+++ app/src/main/java/com/brosedda/mymediary/ui/theme/Type.kt
@@ -2,33 +2,42 @@ package com.brosedda.mymediary.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.brosedda.mymediary.R
+
+val Cabin = FontFamily(
+    Font(R.font.cabin)
+)
+
+val Gotham = FontFamily(
+    Font(R.font.gotham_medium),
+    Font(R.font.gotham_bold, FontWeight.Bold)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )
