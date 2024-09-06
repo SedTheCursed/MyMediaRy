63410a93a1f62a56e1cc9130156d81d0cb89ce86
sedthecused <sed.robert@gmail.com>
2024-09-06 13:34:11 +0200

    Add Libre Baskerville and Metamorphous as fonts

--- app/src/main/java/com/brosedda/mymediary/ui/theme/Type.kt
+++ app/src/main/java/com/brosedda/mymediary/ui/theme/Type.kt
@@ -4,39 +4,41 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.brosedda.mymediary.R
 
-val Cabin = FontFamily(
-    Font(R.font.cabin)
+val Metamorphous = FontFamily(
+    Font(R.font.metamorphous_regular)
 )
 
-val Gotham = FontFamily(
-    Font(R.font.gotham_medium),
-    Font(R.font.gotham_bold, FontWeight.Bold)
+val Baskerville = FontFamily(
+    Font(R.font.libre_baskerville_regular),
+    Font(R.font.libre_baskerville_bold, FontWeight.Bold),
+    Font(R.font.libre_baskerville_italic, FontWeight.Normal, FontStyle.Italic)
 )
 
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = Cabin,
+        fontFamily = Metamorphous,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = Gotham,
+        fontFamily = Metamorphous,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = Gotham,
+        fontFamily = Baskerville,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = Gotham,
+        fontFamily = Baskerville,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     )
