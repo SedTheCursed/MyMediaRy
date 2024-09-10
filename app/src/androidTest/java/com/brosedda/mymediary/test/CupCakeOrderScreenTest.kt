27ff6790d974a41ec44708e19a6f0627ef29c7a9
sedthecused <sed.robert@gmail.com>
2024-09-10 13:51:59 +0200

    Add Cupcake for navigation reference

---
+++ app/src/androidTest/java/com/brosedda/mymediary/test/CupCakeOrderScreenTest.kt
@@ -0,0 +1,57 @@
+package com.brosedda.mymediary.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.brosedda.mymediary.ui.SelectOptionScreen
+import com.brosedda.mymediary.R
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CupCakeOrderScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    // Given list of options
+    private val flavors = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+    // and subtotal
+    private val subtotal = "$100"
+
+    @Before
+    fun setupScreen() {
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal, flavors)
+        }
+    }
+
+    @Test
+    fun selectOptionScreen_verifyContent() {
+        //All options are displayed on the screen
+        flavors.forEach { flavor ->
+            composeTestRule.onNodeWithText(flavor).assertIsDisplayed()
+        }
+
+        // And the subtotal is displayed correctly
+        composeTestRule.let {
+            it.onNodeWithText(
+                it.activity.getString(R.string.subtotal_price, subtotal)
+            ).assertIsDisplayed()
+        }
+
+        //And the next button is disabled
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
+    }
+
+    @Test
+    fun selectOptionsScreen_chooseAnOption_enableTheNextButton() {
+        composeTestRule.let {
+            it.onNodeWithText("Vanilla").performClick()
+            it.onNodeWithStringId(R.string.next).assertIsEnabled()
+        }
+    }
+}
---
+++ app/src/main/java/com/brosedda/mymediary/data/DataSource.kt
@@ -0,0 +1,25 @@
+package com.brosedda.mymediary.data
+
+import com.brosedda.mymediary.R
+
+/**
+ * @param first value is the source ID for the string to display on each button
+ * @param second actual quantity of cupcakes
+ */
+typealias QuantityOptions = Pair<Int, Int>
+
+object DataSource {
+    val flavors = listOf(
+        R.string.vanilla,
+        R.string.chocolate,
+        R.string.red_velvet,
+        R.string.salted_caramel,
+        R.string.coffee
+    )
+
+    val quantityOptions = listOf(
+        QuantityOptions(R.string.one_cupcake, 1),
+        QuantityOptions(R.string.six_cupcakes, 6),
+        QuantityOptions(R.string.twelve_cupcakes, 12)
+    )
+}
