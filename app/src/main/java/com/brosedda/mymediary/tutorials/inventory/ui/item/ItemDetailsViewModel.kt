c2f1b91b166022c3498c0af1c1c03eccabd6d327
sedthecused <sed.robert@gmail.com>
2024-10-23 10:34:23 +0200

    Inventory: Room Tutorial

---
+++ app/src/main/java/com/brosedda/mymediary/tutorials/inventory/ui/item/ItemDetailsViewModel.kt
@@ -0,0 +1,61 @@
+package com.brosedda.mymediary.tutorials.inventory.ui.item
+
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.brosedda.mymediary.tutorials.inventory.data.ItemsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel to retrieve, update and delete an item from the [ItemsRepository]'s data source.
+ */
+class ItemDetailsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: ItemsRepository
+) : ViewModel() {
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemDetailsDestination.itemIdArg])
+    val uiState: StateFlow<ItemDetailsUiState> =
+        repository.getItemStream(itemId)
+            .filterNotNull()
+            .map { ItemDetailsUiState(
+                itemDetails = it.toItemDetails(),
+                outOfStock = it.quantity <= 0
+            ) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ItemDetailsUiState()
+            )
+
+    fun reduceQuantityByOne() {
+        viewModelScope.launch {
+            val currentItem = uiState.value.itemDetails.toItem()
+            if (currentItem.quantity > 0) {
+                repository.updateItem(currentItem.copy(quantity = currentItem.quantity - 1))
+            }
+        }
+    }
+
+    suspend fun deleteItem() {
+            repository.deleteItem(uiState.value.itemDetails.toItem())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * UI state for ItemDetailsScreen
+ */
+data class ItemDetailsUiState(
+    val outOfStock: Boolean = true,
+    val itemDetails: ItemDetails = ItemDetails()
+)
---
+++ app/src/main/java/com/brosedda/mymediary/tutorials/inventory/ui/item/ItemEditViewModel.kt
@@ -0,0 +1,55 @@
+package com.brosedda.mymediary.tutorials.inventory.ui.item
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.brosedda.mymediary.tutorials.inventory.data.ItemsRepository
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel to retrieve and update an item from the [ItemsRepository]'s data source.
+ */
+class ItemEditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: ItemsRepository
+) : ViewModel() {
+
+    /**
+     * Holds current item ui state
+     */
+    var itemUiState by mutableStateOf(ItemUiState())
+        private set
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            itemUiState = repository.getItemStream(itemId)
+                .filterNotNull()
+                .first()
+                .toItemUiState(true)
+        }
+    }
+
+    fun updateUiState(itemDetails: ItemDetails) {
+        itemUiState =
+            ItemUiState(itemDetails = itemDetails, isEntryValid = validateInput(itemDetails))
+    }
+
+    suspend fun updateItem() {
+        if (validateInput(itemUiState.itemDetails)) {
+            repository.updateItem(itemUiState.itemDetails.toItem())
+        }
+    }
+
+    private fun validateInput(uiState: ItemDetails = itemUiState.itemDetails): Boolean {
+        return with(uiState) {
+            name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank()
+        }
+    }
+}
